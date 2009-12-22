10b1669ad8e24f52fc13bc8a45519e55b9745362
tlamy <tlamy@fff143f6-a9c2-4b89-8f93-78cb2c0259b0>
2009-12-22 16:06:08 +0000

    * Delegation now has precedence over wildcard matching   Thanks to Fab for the fix   Fixes bugs #0000038, #0000042
    git-svn-id: https://mydns-ng.svn.sourceforge.net/svnroot/mydns-ng@213 fff143f6-a9c2-4b89-8f93-78cb2c0259b0

--- mydns/trunk/src/mydns/resolve.c
+++ mydns/trunk/src/mydns/resolve.c
@@ -292,6 +292,7 @@ resolve_label(TASK *t, datasection_t section, dns_qtype_t qtype,
   register MYDNS_RR	*rr = NULL;
   taskexec_t		rv = 0;
   int			recurs = wildcard_recursion;
+  char                  *savelabel = label;
 
 #if DEBUG_ENABLED && DEBUG_RESOLVE
   DebugX("resolve", 1, _("%s: resolve_label(%s, %s, %s, %s, %d)"), desctask(t),
@@ -311,6 +312,34 @@ resolve_label(TASK *t, datasection_t section, dns_qtype_t qtype,
 #endif
     return (rv);
   }
+
+  /* No exact match */
+  /* Check for NS delegation */
+  while (*label) {
+    if ((rr = find_rr(t, soa, DNS_QTYPE_NS, label))) {
+      char *newfqdn;
+      if (LASTCHAR(label) == '.') {
+	newfqdn = STRDUP(label);
+      } else {
+	ASPRINTF(&newfqdn, "%s.%s", label, soa->origin);
+      }
+      rv = process_rr(t, AUTHORITY, qtype, newfqdn, soa, label, rr, level);
+      mydns_rr_free(rr);
+      RELEASE(newfqdn);
+      add_authority_ns(t, section, soa, label);
+#if DEBUG_ENABLED && DEBUG_RESOLVE
+      DebugX("resolve", 1, _("%s: resolve_label(%s) returning results %s"), desctask(t),
+	     fqdn, task_exec_name(rv));
+#endif
+      return (rv);
+    }
+    label = strchr(label, '.');
+    if (!label) break;
+    label++;
+  }
+  /* No NS delegation. */
+  /* Restore label, it may have been modified above */
+  label = savelabel;
 
   /*
    * No exact match.
@@ -420,30 +449,6 @@ resolve_label(TASK *t, datasection_t section, dns_qtype_t qtype,
   }
 
  NOWILDCARDMATCH:
-  /* STILL no match - check for NS records for child delegation */
-  while (*label) {
-    if ((rr = find_rr(t, soa, DNS_QTYPE_NS, label))) {
-      char *newfqdn;
-      if (LASTCHAR(label) == '.') {
-	newfqdn = STRDUP(label);
-      } else {
-	ASPRINTF(&newfqdn, "%s.%s", label, soa->origin);
-      }
-      rv = process_rr(t, AUTHORITY, qtype, newfqdn, soa, label, rr, level);
-      mydns_rr_free(rr);
-      RELEASE(newfqdn);
-      add_authority_ns(t, section, soa, label);
-#if DEBUG_ENABLED && DEBUG_RESOLVE
-      DebugX("resolve", 1, _("%s: resolve_label(%s) returning results %s"), desctask(t),
-	     fqdn, task_exec_name(rv));
-#endif
-      return (rv);
-    }
-    label = strchr(label, '.');
-    if (!label) break;
-    label++;
-  }
-
   return (TASK_EXECUTED);
 }
 /*--- resolve_label() ---------------------------------------------------------------------------*/
